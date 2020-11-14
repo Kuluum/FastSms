b844351ef6b05d2ee9f89e8207cce4966fb85200
Kuluum <kuluum@yandex.ru>
2020-11-14 17:58:57 +0300

    Move testing sms list setup in provider

--- FastSms/Application/ApplicationCoordinator.swift
+++ FastSms/Application/ApplicationCoordinator.swift
@@ -15,12 +15,9 @@ final class ApplicationCoordinator: BaseCoordinator {
     }
     
     private func runStartFlow() {
-        let m1 = SmsModel(phoneNumber: "1", smsText: "text1")
-        let m2 = SmsModel(phoneNumber: "2", smsText: "text2")
-        let m3 = SmsModel(phoneNumber: "3", smsText: "text3")
-        let list = SmsListModel(list: [m1, m2, m3])
+        
         let provider = SmsListModelProvider()
-        provider.listModel = list
+        provider.testSetup()
         
         let coordinator = SmsListCoordinator(router: router, provider: provider)
         self.addDependency(coordinator)
--- FastSms/Application/Model/SmsListModelProvider.swift
+++ FastSms/Application/Model/SmsListModelProvider.swift
@@ -13,4 +13,14 @@ class SmsListModelProvider {
     func append(_ smsModel: SmsModel) {
         listModel = listModel?.appending(sms: smsModel)
     }
+    
+    func testSetup() {
+        let m1 = SmsModel(phoneNumber: "1", smsText: "text1")
+        let m2 = SmsModel(phoneNumber: "2", smsText: "text2")
+        let m3 = SmsModel(phoneNumber: "3", smsText: "text3")
+        let list = SmsListModel(list: [m1, m2, m3])
+        
+        self.listModel = list
+        
+    }
 }
